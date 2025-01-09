50380891c10adae15dcb18dc173d5f722b3256b4
Michael Patrick <michael.patrick.org@gmail.com>
2025-01-09 14:03:06 +0000

    created the navbar with a slide untop

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div className='container py-4 d-flex justify-content-center'>
+        <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
+          <div className="carousel-inner text-light">
+            <div className="carousel-item active">
+              <h4>Matthew 6:33: "You are Favoured"</h4>
+            </div>
+            <div className="carousel-item">
+              <h4>Matthew 6:33: "You are Great"</h4>
+            </div>
+            <div className="carousel-item">
+              <h4>Matthew 6:33: "You are Free"</h4>
+            </div>
+            <div className="carousel-item">
+              <h4>Matthew 6:33: "You are Sanctified"</h4>
+            </div>
+            <div className="carousel-item">
+              <h4>Matthew 6:33: "You are Loved"</h4>
+            </div>
+            <div className="carousel-item">
+              <h4>Matthew 6:33: "You are Saved"</h4>
+            </div>
+            <div className="carousel-item">
+              <h4>Matthew 6:33: "You are Empower"</h4>
+            </div>
+            <div className="carousel-item">
+              <h4>Matthew 6:33: "You are Enthroned"</h4>
+            </div>
+            <div className="carousel-item">
+              <h4>Matthew 6:33: "You are Enlighten"</h4>
+            </div>
+            <div className="carousel-item">
+              <h4>Matthew 6:33: "You are Healed"</h4>
+            </div>
+            <div className="carousel-item">
+              <h4>You are lifted</h4>
+            </div>
+            <div className="carousel-item">
+              <h4>You are blessed</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <>
+        <nav className="navbar navbar-expand-lg navbar-fixed-top">
+            <div className="container py-2 bg-light">
+                <a className="navbar-brand" href="#">Kingdom Promoters</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav ms-auto h4">
+                    <li className="nav-item">
+                    <a className="nav-link" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link" href="#">About</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link" href="#">Services</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link" href="#">Projects</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link" href="#">Testimonies</a>
+                    </li>
+                    <li className="nav-item">
+                        <button className='btn btn-warning'>
+                            <a className="nav-link" href="#">Contact</a>
+                        </button>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </nav>
+        
+    </>
+  )
+}
+
+export default NavBar
