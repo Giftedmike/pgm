95d6b2b54cd81ab04c00ead5e52d3dfabce91be8
Michael Patrick <michael.patrick.org@gmail.com>
2025-01-23 15:31:11 +0000

    added the footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <div className='text-light text-center pt-3 h5'>
+        <footer>
+          <a href="#" className='pe-2'>
+            About Us
+          </a>|| 
+          <a href="#" className='px-2'>
+            Privacy Policy
+          </a>||
+          <a href="#" className='ps-2'>
+            Volunteering
+          </a> ||
+          <a href="#" className='ps-2'>
+            Back To Top
+          </a>
+
+          <p className='pt-2'>Author: Kingdom Promoters || &copy; Some rights reserved</p>
+        </footer>
+      </div>
+    </>
   )
 }
 
